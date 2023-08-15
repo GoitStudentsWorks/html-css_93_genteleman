5effb0cf5504ce3790828de41c67ac68c275e0ae
VitaliiShc <vitaliy.shchukin@gmail.com>
2023-08-15 08:20:54 +0300

    add script for menu

---
+++ src/js/mobile-menu.js
@@ -0,0 +1,16 @@
+(() => {
+  const refs = {
+    openMenuBtn: document.querySelector('[mob-menu-open]'),
+    closeMenuBtn: document.querySelector('[mob-menu-close]'),
+    menu: document.querySelector('[mob-menu]'),
+  };
+
+  refs.openMenuBtn.addEventListener('click', toggleMenu);
+  refs.closeMenuBtn.addEventListener('click', toggleMenu);
+
+  function toggleMenu() {
+    refs.menu.classList.toggle('is-hidden');
+    refs.openMenuBtn.classList.toggle('is-hidden');
+    document.body.classList.toggle('no-scroll');
+  }
+})();
